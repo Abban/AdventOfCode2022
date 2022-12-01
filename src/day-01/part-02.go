8b6d6ef268daecd0181b6d1b5d0ee662ec61fed5
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-01 09:45:55 +0100

    Add day 01 part 02

--- src/day-01/part-02.go
+++ src/day-01/part-02.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,18 +13,20 @@ func main() {
 	data, _ := os.ReadFile(dir + "/src/day-01/input.txt")
 	dataArray := strings.Split(string(data), "\n")
 	number := 0
-	highest := 0
+	combined := make([]int, 0)
 
 	for i := 0; i < len(dataArray); i++ {
 		if dataArray[i] == "" || i == len(dataArray)-1 {
-			if number > highest {
-				highest = number
-			}
+			combined = append(combined, number)
 			number = 0
 		}
 		numberToAdd, _ := strconv.ParseInt(dataArray[i], 10, 64)
 		number += int(numberToAdd)
 	}
 
-	fmt.Println("HIGHEST: ", highest)
+	sort.Slice(combined, func(p, q int) bool {
+		return combined[p] > combined[q]
+	})
+
+	fmt.Println("HIGHEST: ", combined[0]+combined[1]+combined[2])
 }
